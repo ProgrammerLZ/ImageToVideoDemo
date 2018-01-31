690e08dae43c21824f691190690a882525bb90af
刘哲 <liuzhe@liuzhedemacbook-pro.local>
2018-01-31 10:14:00 +0800

    Initial Commit

---
+++ ImageToVideoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageToVideoDemo
+//
+//  Created by 刘哲 on 2018/1/31.
+//  Copyright © 2018年 刘哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageToVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageToVideoDemo
+//
+//  Created by 刘哲 on 2018/1/31.
+//  Copyright © 2018年 刘哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
